1794fba22ab32e188c2756c85794a143a6a9e06c
lvanxiang <2135261459>
2019-08-08 18:23:34 +0800

    Initial commit

---
+++ src/H.java
@@ -0,0 +1,12 @@
+public class H {
+public static  void main(String[]args){
+        System.out.println("hhhhhhhhhh");
+        System.out.println("helloeWorldni你好");
+int num=10;
+        System.out.println("num = " + num);
+}
+}
+
+class QA {
+
+}
---
+++ src/Hellow.java
@@ -0,0 +1,6 @@
+/**
+ * @author lvanxiang
+ * @date 2019/8/8 - 9:10
+ */
+public class Hellow {
+}
